bca9fc0f2ef74758c4304e0533f06b82281bb508
Swapnil Dubey <100476652+swapnildubey29@users.noreply.github.com>
2024-04-02 15:11:44 +0530

    Update server.js

--- Stock Web/server.js
+++ Stock Web/server.js
@@ -1,14 +1,14 @@
-const express = require('express');
-const axios = require('axios');
-const app = express();
+const express = require('express')
+const axios = require('axios')
+const app = express()
 
 const API_KEY = 'eb27a22-a6fa-478f-8df6-066183004091';
 const API_SECRET = 'o2ex8m7hno';
 
-app.use(express.json());
-app.use(express.static('client'));
+app.use(express.json())
+app.use(express.static('client'))
 
-// Function to fetch data from Upstox API
+//Function to fetch data from Upstox API
 async function fetchData(Ikey, interval, toDate, fromDate) {
   const baseURL = 'https://api.upstox.com/v2/historical-candle/';
 
@@ -20,7 +20,7 @@ async function fetchData(Ikey, interval, toDate, fromDate) {
       },
     });
 
-    return response.data;
+    return response.data
   } catch (error){
     console.error('Error fetching data:', error);
     throw new Error('Failed to fetch data');
@@ -28,17 +28,17 @@ async function fetchData(Ikey, interval, toDate, fromDate) {
 }
 
 app.get('/historical-candle/:Ikey/:interval/:toDate/:fromDate', async (req, res) => {
-  const {Ikey, interval, toDate, fromDate } = req.params;
+  const {Ikey, interval, toDate, fromDate } = req.params
 
   try{
-    const data = await fetchData(Ikey, interval, toDate, fromDate);
-    console.log(data);
-    res.json(data);
+    const data = await fetchData(Ikey, interval, toDate, fromDate)
+    console.log(data)
+    res.json(data)
   } catch (error) {
-    console.error('Error fetching data:', error);
-    res.status(500).json({ error: 'Failed to fetch data' });
+    console.error('Error fetching data:', error)
+    res.status(500).json({ error: 'Failed to fetch data' })
   }
-});
+})
 
 
-app.listen(8000);
+app.listen(8000)
